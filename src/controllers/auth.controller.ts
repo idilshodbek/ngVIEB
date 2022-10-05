cbb15575b562c517a8def73d9dad2b6933e552a2
Ismoilov Dilshodbek <idilshodbek23@gmail.com>
2022-10-05 18:21:39 +0500

    test

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import AuthService from '../services/auth.service';
 
+
 class AuthController {
     public authService = new AuthService;
 
